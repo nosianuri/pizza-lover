89a09066cd78e2d61170d2f7babc6b5e7d1cf107
nosianuri <nurinosia@gmail.com>
2022-09-10 18:30:22 +0600

    set footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,22 @@
+import {UilFacebook, UilGithub, UilInstagram} from "@iconscout/react-unicons"
+import Image from 'next/image'
+import Logo from '../assets/Logo.png'
+import css from '../styles/Footer.module.css'
+
 export default function Footer() {
     return(
-        <div>
-            Footer
+        <div className={css.container}>
+            <span>ALL RIGHT RESERVED</span>
+            <div className={css.social}>
+                <UilFacebook size={45}/>
+                <UilGithub size={45}/>
+                <UilInstagram size={45}/>
+            </div>
+
+            <div className="css.logo">
+                <Image src={Logo} alt="" size={45} />
+                <span>Fudo</span>
+            </div>
         </div>
     )
-};
+}
