c63cea8c83f113e7fd4249a6873ebb718558399b
nosianuri <nurinosia@gmail.com>
2022-11-07 14:20:34 +0600

    add services

---
+++ components/Services.jsx
@@ -0,0 +1,13 @@
+import css from "../styles/Services.module.css";
+
+export default function Services() {
+    return(
+        <>
+            <div className={css.heading}>
+                <span>WHAT WE SERVE</span>
+                <span>YOUR FAVOURITE FOOD</span>
+                <span>DELIVERY PARTNER</span>
+            </div>
+        </>
+    )
+}
