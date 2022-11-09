ad9524c41137609233ea8cfedad8380383777220
nosianuri <nurinosia@gmail.com>
2022-11-09 13:20:29 +0600

    update online payment method

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-## Pizza Lover
+## Pizza Lover Website with Payment method
 
 [Live Link](https://pizza-lover.vercel.app/)
--- pages/cart.js
+++ pages/cart.js
@@ -1,33 +1,56 @@
 import Image from "next/image";
 import { useState } from "react";
-import toast, {Toaster} from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 import Layout from "../components/Layout";
 import OrderModal from "../components/OrderModal";
 import { urlFor } from "../lib/client";
 import { useStore } from "../store/store";
 import css from '../styles/Cart.module.css';
+import { useRouter } from 'next/router';
 
 export default function Cart() {
-    const CartData = useStore((state)=>state.cart);
-    const removePizza = useStore((state)=>state.removePizza);
-    const [PaymentMethod, setPaymentMethod] = useState(null)
+    const CartData = useStore((state) => state.cart);
+    const removePizza = useStore((state) => state.removePizza);
+    const [PaymentMethod, setPaymentMethod] = useState(null);
+    const [Order, setOrder] = useState(
+        typeof window !== "undefined" && localStorage.getItem('order')
+    )
     const handleRemove = (i) => {
         removePizza(i);
         toast.error('Item Removed')
     };
-
-    const total = ()=> CartData.pizzas.reduce((a,b)=>a+b.quantity * b.price, 0)
+    const router = useRouter()
+    const total = () => CartData.pizzas.reduce((a, b) => a + b.quantity * b.price, 0)
 
     const handleOnDelivery = () => {
         setPaymentMethod(0);
         typeof window !== 'undefined' && localStorage.setItem('total', total())
 
     }
-    return(
+
+    const handleCheckout = async () => {
+        typeof window !== 'undefined' && localStorage.setItem('total', total())
+        setPaymentMethod(1);
+        const response = await fetch("/api/stripe", {
+            method: "POST",
+            headers: {
+                'Content-Type': "application/json",
+
+            },
+            body: JSON.stringify(CartData.pizzas),
+        });
+
+        if(response.status === 500) return;
+
+        const data = await response.json();
+        toast.loading("Redirecting...");
+        router.push(data.url)
+    }
+    return (
         <Layout>
             <div className={css.container}>
 
-            {/* details */}
+                {/* details */}
                 <div className={css.details}>
                     <table className={css.table}>
                         <thead>
@@ -41,48 +64,50 @@ export default function Cart() {
                         </thead>
                         <tbody className={css.tbody}>
                             {CartData.pizzas.length > 0 &&
-                            CartData.pizzas.map((pizza, i)=> {
+                                CartData.pizzas.map((pizza, i) => {
 
-                                const src = urlFor(pizza.image).url()
-                                return(
-                                <tr key={i}>
-                                    <td className={css.imageTd}>
-                                        <Image 
-                                            loader = {()=> src}
-                                            
-                                            src={src}
-                                            alt=""
-                                            objectFit="cover"
-                                            width={85}
-                                            height={85}
-                                        />
-                                    </td>
-                                    <td>
-                                        {pizza.name}
-                                    </td>
-                                    <td>
-                                        {
-                                            pizza.size === 0 ?
-                                            "small" :
-                                            pizza.size === 1 ?
-                                            "Medium" :
-                                            "Large"
-                                        }
-                                    </td>
+                                    const src = urlFor(pizza.image).url()
+                                    return (
+                                        <tr key={i}>
+                                            <td className={css.imageTd}>
+                                                <Image
+                                                    loader={() => src}
+                                                    src={src}
+                                                    alt=""
+                                                    objectFit="cover"
+                                                    width={85}
+                                                    height={85}
+                                                />
+                                            </td>
+                                            <td>
+                                                {pizza.name}
+                                            </td>
+                                            <td>
+                                                {
+                                                    pizza.size === 0 ?
+                                                        "small" :
+                                                        pizza.size === 1 ?
+                                                            "Medium" :
+                                                            "Large"
+                                                }
+                                            </td>
 
-                                    <td>
-                                        {pizza.price}
-                                    </td>
-                                    <td>
-                                        {pizza.quantity}
-                                    </td>
-                                    <td>
-                                        {pizza.price * pizza.quantity}
-                                    </td>
-                                    <td style={{color: "var(--themeRed)",
-                                    cursor: 'pointer'}} onClick={() =>handleRemove(i)}>X</td>
-                                </tr>
-                                )})}
+                                            <td>
+                                                {pizza.price}
+                                            </td>
+                                            <td>
+                                                {pizza.quantity}
+                                            </td>
+                                            <td>
+                                                {pizza.price * pizza.quantity}
+                                            </td>
+                                            <td style={{
+                                                color: "var(--themeRed)",
+                                                cursor: 'pointer'
+                                            }} onClick={() => handleRemove(i)}>X</td>
+                                        </tr>
+                                    )
+                                })}
                         </tbody>
                     </table>
                 </div>
@@ -102,18 +127,21 @@ export default function Cart() {
                         </div>
                     </div>
 
-                    <div className={css.buttons}>
-                        <button className="btn" onClick={handleOnDelivery}>Pay on Delivery</button>
-                        <button className="btn">Pay Now</button>
-                    </div>
+                    {!Order && CartData.pizzas.length > 0 ?
+                        (<div className={css.buttons}>
+                            <button className="btn" onClick={handleOnDelivery}>Pay on Delivery</button>
+                            <button className="btn" onClick={handleCheckout}>Pay Now</button>
+                        </div>) : null
+                    }
+
                 </div>
             </div>
             <Toaster />
 
             {/* Modal */}
-            <OrderModal opened = {PaymentMethod === 0} 
-            setOpened = {setPaymentMethod}
-            PaymentMethod = {PaymentMethod} />
+            <OrderModal opened={PaymentMethod === 0}
+                setOpened={setPaymentMethod}
+                PaymentMethod={PaymentMethod} />
         </Layout>
     )
 }
